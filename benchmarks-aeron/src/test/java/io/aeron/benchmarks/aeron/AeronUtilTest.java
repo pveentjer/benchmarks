2532bc30da426001f65c87404f90b2b91fa74709
Dmytro Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2025-06-26 16:47:22 +0100

    [Java] Fix Checkstyle warnings.

--- benchmarks-aeron/src/test/java/io/aeron/benchmarks/aeron/AeronUtilTest.java
+++ benchmarks-aeron/src/test/java/io/aeron/benchmarks/aeron/AeronUtilTest.java
@@ -479,6 +479,7 @@ class AeronUtilTest
         assertFalse(Files.exists(errorsFile));
     }
 
+    @SuppressWarnings({ "Indentation" })
     @Test
     void dumpClusterErrorsSavesErrorsRecordedInTheMarkFile(
         @TempDir final Path resultsDir,
